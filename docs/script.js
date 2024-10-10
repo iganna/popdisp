9dd35b5bccb55dd94bf0125eb25749dc47a74271
Anna Igolkina <igolkinaanna11@gmail.com>
2024-10-10 12:11:22 +0200

    good

--- docs/script.js
+++ docs/script.js
@@ -3,4 +3,9 @@ document.querySelectorAll('.dropdown-btn').forEach(button => {
         this.classList.toggle('active');  // Добавляем или убираем класс active для поворота галочки
         let dropdownContent = this.nextElementSibling;
         if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display 
+            dropdownContent.style.display = "none"; // Скрываем подменю
+        } else {
+            dropdownContent.style.display = "block"; // Показываем подменю
+        }
+    });
+});
